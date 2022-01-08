8c81be2e1ac31eb05a0849c3369f8403eb27d5c8
Mahesh Tamkhade <tamkhade21@gmail.com>
2022-01-08 17:41:45 +0530

    added

--- src/test/java/com/jenkins/demo/JenkinsRestApplicationTests.java
+++ src/test/java/com/jenkins/demo/JenkinsRestApplicationTests.java
@@ -1,22 +1,22 @@
-package com.jenkins.demo;
-
-import static org.junit.Assert.assertEquals;
-
-import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.test.context.SpringBootTest;
-
-
-@SpringBootTest
-class JenkinsRestApplicationTests {
-
-	public  Logger logger = LoggerFactory.getLogger(JenkinsRestApplicationTests.class);
-	@Test
-	void contextLoads() {
-		logger.info("-----------In Test case execution ----");
-		
-		assertEquals(true, true);
-	}
-
-}
+/*
+ * package com.jenkins.demo;
+ * 
+ * import static org.junit.Assert.assertEquals;
+ * 
+ * import org.junit.jupiter.api.Test; import org.slf4j.Logger; import
+ * org.slf4j.LoggerFactory; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * 
+ * @SpringBootTest class JenkinsRestApplicationTests {
+ * 
+ * public Logger logger =
+ * LoggerFactory.getLogger(JenkinsRestApplicationTests.class);
+ * 
+ * @Test void contextLoads() {
+ * logger.info("-----------In Test case execution ----");
+ * 
+ * assertEquals(true, true); }
+ * 
+ * }
+ */
