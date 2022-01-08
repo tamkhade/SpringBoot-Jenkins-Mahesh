3fc261937d08ee9054c43278e01c317385170711
Mahesh Tamkhade <tamkhade21@gmail.com>
2022-01-08 16:36:18 +0530

    Remove RunWith annotation

--- src/test/java/com/jenkins/demo/JenkinsRestApplicationTests.java
+++ src/test/java/com/jenkins/demo/JenkinsRestApplicationTests.java
@@ -3,14 +3,11 @@ package com.jenkins.demo;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 class JenkinsRestApplicationTests {
 
